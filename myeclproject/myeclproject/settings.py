47d4cc52b9f6ee5e98b1b5e45d323be4e7603c01
Vijay-Kumar-Reddy <vijaymarripati7@gmail.com>
2025-11-27 13:49:36 -0500

    final settings update

--- myeclproject/myeclproject/settings.py
+++ myeclproject/myeclproject/settings.py
@@ -26,10 +26,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6r3(lf^7(a!0)wx%_&x@7j6z%g8+3q#ghozou+b1rn9u8^bpz$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://localhost','https://ecl-production.up.railway.app', 'https://eilandcigarlounge.com/']
+ALLOWED_HOSTS = [
+    "ecl-production.up.railway.app",
+    "eilandcigarlounge.com",
+    "www.eilandcigarlounge.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "https://ecl-production.up.railway.app/",
+    "https://eilandcigarlounge.com/",
+]
+
 
 
 # Application definition
